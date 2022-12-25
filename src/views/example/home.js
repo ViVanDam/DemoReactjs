a913430cf60409897833111dd264b1070d7a114b
ViVanDam <vidam21042001@gmail.com>
2022-12-25 22:54:06 +0700

    add image to web

--- src/views/example/home.js
+++ src/views/example/home.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-
+import coffeShop from "../../assets/images/quanCafetrongLonBia.jpg";
 class Home extends React.Component {
-  componentDidMount() {
-    setTimeout(() => {
-      this.props.history.push("/todos");
-    }, 3000);
-  }
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.props.history.push("/todos");
+  //   }, 3000);
+  // }
 
   render() {
     return (
       <>
         <p>Hello This is Home page</p>
         <p>My name is Adam.</p>
+        <img className="coffee" src={coffeShop}></img>
       </>
     );
   }
